232fe3885d5e395a4e922322a62e73ce8590934f
macro <maczh@qq.com>
2025-11-12 09:40:22 +0800

    修改

--- test/test_utils.py
+++ test/test_utils.py
@@ -7,14 +7,15 @@ sys.path.insert(0, project_root)
 
 from utils.base64util import base64_encode, base64_decode
 from utils.desutil import *
+from utils.aesutil import *
 
 if __name__ == "__main__":
     original = "Hello, World!"
-    encoded = base64_encode(original)
-    print(encoded)
-    decoded = base64_decode(encoded)
-    print(decoded)
-    crypted = des_encrypt_str(original, "12345678")
+    # encoded = base64_encode(original)
+    # print(encoded)
+    # decoded = base64_decode(encoded)
+    # print(decoded)
+    crypted = aes_encrypt_base64(original.encode(), b'1234567890123456',iv=b'1234567890123456', mode="CBC", padding="pkcs5")
     print(crypted)
-    decrypted = des_decrypt_str(crypted, "12345678")
-    print(decrypted)
+    decrypted = aes_decrypt_base64(crypted, b"1234567890123456",iv=b'1234567890123456',mode="CBC", padding="pkcs5")
+    print(decrypted.decode())
